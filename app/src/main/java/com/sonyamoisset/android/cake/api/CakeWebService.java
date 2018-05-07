97c2a791b0479020ace55c736621692ad876dd4e
smoisset <smoisset@worldremit.com>
2018-05-07 19:12:35 +0100

    Create CakeWebService

--- app/src/main/java/com/sonyamoisset/android/cake/api/CakeWebService.java
+++ app/src/main/java/com/sonyamoisset/android/cake/api/CakeWebService.java
@@ -1,4 +1,14 @@
 package com.sonyamoisset.android.cake.api;
 
+import android.arch.lifecycle.LiveData;
+
+import com.sonyamoisset.android.cake.db.entity.RecipeEntity;
+
+import java.util.List;
+
+import retrofit2.http.GET;
+
 public interface CakeWebService {
+    @GET("baking.json")
+    LiveData<ApiResponse<List<RecipeEntity>>> loadRecipes();
 }
