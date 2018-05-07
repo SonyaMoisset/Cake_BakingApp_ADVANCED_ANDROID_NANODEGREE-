d2242039a94c7420c7e25904d61f86f004d33ae9
smoisset <smoisset@worldremit.com>
2018-05-07 18:27:25 +0100

    Add AppDatabase

--- app/src/main/java/com/sonyamoisset/android/cake/db/AppDataBase.java
+++ app/src/main/java/com/sonyamoisset/android/cake/db/AppDataBase.java
@@ -1,4 +1,17 @@
 package com.sonyamoisset.android.cake.db;
 
-public class AppDataBase {
+import android.arch.persistence.room.Database;
+import android.arch.persistence.room.RoomDatabase;
+
+import com.sonyamoisset.android.cake.db.dao.RecipeDao;
+import com.sonyamoisset.android.cake.db.entity.IngredientEntity;
+import com.sonyamoisset.android.cake.db.entity.RecipeEntity;
+import com.sonyamoisset.android.cake.db.entity.StepEntity;
+
+@Database(entities = {RecipeEntity.class, IngredientEntity.class, StepEntity.class}, version = 1)
+public abstract class AppDataBase extends RoomDatabase {
+
+    public static final String DATABASE_NAME = "cake";
+
+    abstract public RecipeDao recipeDao();
 }
