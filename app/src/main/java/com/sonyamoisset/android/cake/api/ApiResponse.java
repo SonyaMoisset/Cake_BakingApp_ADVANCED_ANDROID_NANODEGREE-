818c4941c6e5a33f64cef8cb4b700134e3f16741
smoisset <smoisset@worldremit.com>
2018-05-07 19:08:37 +0100

    Define ApiResponse

--- app/src/main/java/com/sonyamoisset/android/cake/api/ApiResponse.java
+++ app/src/main/java/com/sonyamoisset/android/cake/api/ApiResponse.java
@@ -1,4 +1,47 @@
 package com.sonyamoisset.android.cake.api;
 
-public class ApiResponse {
+import java.io.IOException;
+
+import retrofit2.Response;
+
+public class ApiResponse<T> {
+    public final int code;
+    public final T body;
+    public final String errorMessage;
+
+    public boolean isSuccessful() {
+        return code >= 200 && code < 300;
+    }
+
+    public ApiResponse(Response<T> response) {
+        code = response.code();
+
+        if (response.isSuccessful()) {
+            body = response.body();
+            errorMessage = null;
+        } else {
+            String message = null;
+
+            if (response.errorBody() != null) {
+                try {
+                    message = response.errorBody().string();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            if (message == null || message.trim().length() == 0) {
+                message = response.message();
+            }
+
+            errorMessage = message;
+            body = null;
+        }
+    }
+
+    public ApiResponse(Throwable error) {
+        code = 500;
+        body = null;
+        errorMessage = error.getMessage();
+    }
 }
