9a71656069d9d0380b9407b3f4574cf02f5d1a5e
smoisset <smoisset@worldremit.com>
2018-05-07 18:15:07 +0100

    Add FullRecipe POJO Model

--- app/src/main/java/com/sonyamoisset/android/cake/db/entity/FullRecipeEntity.java
+++ app/src/main/java/com/sonyamoisset/android/cake/db/entity/FullRecipeEntity.java
@@ -1,4 +1,19 @@
 package com.sonyamoisset.android.cake.db.entity;
 
+import android.arch.persistence.room.Embedded;
+import android.arch.persistence.room.Relation;
+
+import java.util.List;
+
+@SuppressWarnings("unused")
 public class FullRecipeEntity {
+
+    @Embedded
+    public RecipeEntity recipe;
+
+    @Relation(parentColumn = "id", entityColumn = "recipe_id", entity = IngredientEntity.class)
+    public List<IngredientEntity> ingredients;
+
+    @Relation(parentColumn = "id", entityColumn = "recipe_id", entity = StepEntity.class)
+    public List<StepEntity> steps;
 }
