0e2f2b135b108a2396f33d9beaaa5c64e0571d2c
smoisset <smoisset@worldremit.com>
2018-05-07 20:19:42 +0100

    Define ViewModelModule

--- app/src/main/java/com/sonyamoisset/android/cake/di/ViewModelModule.java
+++ app/src/main/java/com/sonyamoisset/android/cake/di/ViewModelModule.java
@@ -1,4 +1,29 @@
 package com.sonyamoisset.android.cake.di;
 
-public class ViewModelModule {
+import android.arch.lifecycle.ViewModel;
+import android.arch.lifecycle.ViewModelProvider;
+
+import com.sonyamoisset.android.cake.ui.detail.DetailViewModel;
+import com.sonyamoisset.android.cake.ui.main.MainViewModel;
+import com.sonyamoisset.android.cake.viewmodel.ViewModelFactory;
+
+import dagger.Binds;
+import dagger.Module;
+import dagger.multibindings.IntoMap;
+
+@Module
+public abstract class ViewModelModule {
+
+    @Binds
+    @IntoMap
+    @ViewModelKey(MainViewModel.class)
+    abstract ViewModel bindMainViewModel(MainViewModel mainViewModel);
+
+    @Binds
+    @IntoMap
+    @ViewModelKey(DetailViewModel.class)
+    abstract ViewModel bindDetailViewModel(DetailViewModel detailViewModel);
+
+    @Binds
+    abstract ViewModelProvider.Factory bindViewModelFactory(ViewModelFactory viewModelFactory);
 }
