48fbda44da5ff4e1611a43b97689a9d99a2bf358
smoisset <smoisset@worldremit.com>
2018-05-07 18:27:02 +0100

    Add RecipeDao

--- app/src/main/java/com/sonyamoisset/android/cake/db/dao/RecipeDao.java
+++ app/src/main/java/com/sonyamoisset/android/cake/db/dao/RecipeDao.java
@@ -1,4 +1,54 @@
 package com.sonyamoisset.android.cake.db.dao;
 
-public class RecipeDao {
+import android.arch.lifecycle.LiveData;
+import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.Query;
+import android.arch.persistence.room.Transaction;
+
+import com.sonyamoisset.android.cake.db.entity.FullRecipeEntity;
+import com.sonyamoisset.android.cake.db.entity.IngredientEntity;
+import com.sonyamoisset.android.cake.db.entity.RecipeEntity;
+import com.sonyamoisset.android.cake.db.entity.StepEntity;
+
+import java.util.List;
+
+@Dao
+public abstract class RecipeDao {
+
+    @Insert
+    public abstract void _insertRecipes(List<RecipeEntity> recipes);
+
+    @Insert
+    public abstract void _insertIngredients(List<IngredientEntity> ingredients);
+
+    @Insert
+    public abstract void _insertSteps(List<StepEntity> steps);
+
+    public void insertRecipes(List<RecipeEntity> recipes) {
+        for (RecipeEntity recipe : recipes) {
+
+            List<IngredientEntity> ingredients = recipe.getIngredients();
+            for (IngredientEntity ingredient : ingredients) {
+                ingredient.setRecipeId(recipe.getId());
+            }
+            _insertIngredients(ingredients);
+
+            List<StepEntity> steps = recipe.getSteps();
+            for (StepEntity step : steps) {
+                step.setRecipeId(recipe.getId());
+            }
+            _insertSteps(steps);
+        }
+
+        _insertRecipes(recipes);
+    }
+
+    @Transaction
+    @Query("SELECT * FROM recipe")
+    public abstract LiveData<List<FullRecipeEntity>> loadRecipes();
+
+    @Transaction
+    @Query("SELECT * FROM recipe WHERE id = :id")
+    public abstract FullRecipeEntity loadRecipe(int id);
 }
