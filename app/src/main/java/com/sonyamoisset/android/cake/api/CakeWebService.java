2844295f3bc196dd0e17f349351e2ec2e1f9cb3a
smoisset <smoisset@worldremit.com>
2018-05-10 23:58:40 +0100

    Refactor API files

--- app/src/main/java/com/sonyamoisset/android/cake/api/CakeWebService.java
+++ app/src/main/java/com/sonyamoisset/android/cake/api/CakeWebService.java
@@ -2,7 +2,7 @@ package com.sonyamoisset.android.cake.api;
 
 import android.arch.lifecycle.LiveData;
 
-import com.sonyamoisset.android.cake.db.entity.RecipeEntity;
+import com.sonyamoisset.android.cake.db.entity.Recipe;
 
 import java.util.List;
 
@@ -10,5 +10,5 @@ import retrofit2.http.GET;
 
 public interface CakeWebService {
     @GET("baking.json")
-    LiveData<ApiResponse<List<RecipeEntity>>> loadRecipes();
+    LiveData<ApiResponse<List<Recipe>>> getRecipes();
 }
