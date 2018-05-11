6e29d73384ce6ce90f57840ef810c184c8e72570
smoisset <smoisset@worldremit.com>
2018-05-11 01:47:46 +0100

    Implement DataBinding for RecipeAdapter

--- app/src/main/java/com/sonyamoisset/android/cake/ui/main/RecipeAdapter.java
+++ app/src/main/java/com/sonyamoisset/android/cake/ui/main/RecipeAdapter.java
@@ -1,47 +1,38 @@
 package com.sonyamoisset.android.cake.ui.main;
 
-import android.content.Context;
+import android.databinding.DataBindingUtil;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
-import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.TextView;
 
 import com.sonyamoisset.android.cake.R;
+import com.sonyamoisset.android.cake.databinding.ActivityRecipeListItemBinding;
 import com.sonyamoisset.android.cake.db.entity.Recipe;
 import com.squareup.picasso.Picasso;
 
 import java.util.List;
 
-import butterknife.BindView;
-import butterknife.ButterKnife;
-
 public class RecipeAdapter extends RecyclerView.Adapter<RecipeAdapter.RecipeViewHolder> {
 
-    private final Context context;
     private List<Recipe> recipes;
+    private ActivityRecipeListItemBinding binding;
 
     class RecipeViewHolder extends RecyclerView.ViewHolder {
+        final ActivityRecipeListItemBinding binding;
 
-        @BindView(R.id.activity_recipe_list_item_placeholder_image)
-        ImageView recipeThumbnailImage;
-        @BindView(R.id.activity_recipe_list_item_name)
-        TextView recipeName;
-        @BindView(R.id.activity_recipe_list_item_servings_count)
-        TextView recipeServingsCount;
-        @BindView(R.id.activity_recipe_list_item_ingredients_steps_count)
-        TextView recipeIngredientsStepsCount;
+        RecipeViewHolder(ActivityRecipeListItemBinding binding) {
+            super(binding.getRoot());
+            this.binding = binding;
+        }
 
-        public RecipeViewHolder(View view) {
-            super(view);
-            ButterKnife.bind(this, view);
+        void bind(Recipe recipe) {
+            binding.setRecipe(recipe);
         }
     }
 
-    public RecipeAdapter(Context context, List<Recipe> recipes) {
-        this.context = context;
+    RecipeAdapter(List<Recipe> recipes) {
         this.recipes = recipes;
     }
 
@@ -53,21 +44,24 @@ public class RecipeAdapter extends RecyclerView.Adapter<RecipeAdapter.RecipeView
     @NonNull
     @Override
     public RecipeViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext())
-                .inflate(R.layout.activity_recipe_list_item, parent, false);
-        return new RecipeViewHolder(view);
+        LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
+        binding = DataBindingUtil.inflate(layoutInflater, R.layout.activity_recipe_list_item, parent, false);
+        return new RecipeViewHolder(binding);
     }
 
     @Override
     public void onBindViewHolder(@NonNull RecipeViewHolder holder, int position) {
         Recipe recipe = recipes.get(position);
-        holder.recipeName.setText(recipe.getName());
-        holder.recipeServingsCount.setText(String.format("%d", recipe.getServings()));
-        holder.recipeIngredientsStepsCount.setText(String.format("%d", recipe.getSteps().size()));
+        holder.bind(recipe);
 
-        Picasso.get()
-                .load(R.drawable.cake)
-                .into(holder.recipeThumbnailImage);
+        if (!TextUtils.isEmpty(recipe.getImage())) {
+            Picasso.get()
+                    .load(recipe.getImage())
+                    .into(binding.activityRecipeListItemPlaceholderImage);
+        } else {
+            binding.activityRecipeListItemPlaceholderImage.setImageResource(R.drawable.cake);
+        }
+
     }
 
     @Override
