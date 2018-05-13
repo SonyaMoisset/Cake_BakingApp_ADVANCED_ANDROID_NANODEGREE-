6702e723a15c2f04c2e091e9f561d3883a350d9b
smoisset <smoisset@worldremit.com>
2018-05-13 22:54:29 +0100

    Refactor VIEWMODEL file

--- app/src/main/java/com/sonyamoisset/android/cake/viewmodel/CakeViewModelFactory.java
+++ app/src/main/java/com/sonyamoisset/android/cake/viewmodel/CakeViewModelFactory.java
@@ -2,6 +2,7 @@ package com.sonyamoisset.android.cake.viewmodel;
 
 import android.arch.lifecycle.ViewModel;
 import android.arch.lifecycle.ViewModelProvider;
+import android.support.annotation.NonNull;
 
 import java.util.Map;
 
@@ -15,25 +16,30 @@ public class CakeViewModelFactory implements ViewModelProvider.Factory {
     private final Map<Class<? extends ViewModel>, Provider<ViewModel>> creators;
 
     @Inject
-    public CakeViewModelFactory(Map<Class<? extends ViewModel>, Provider<ViewModel>> creators) {
+    CakeViewModelFactory(Map<Class<? extends ViewModel>, Provider<ViewModel>> creators) {
         this.creators = creators;
     }
 
+    @NonNull
     @SuppressWarnings("unchecked")
     @Override
-    public <T extends ViewModel> T create(Class<T> modelClass) {
+    public <T extends ViewModel> T create(@NonNull Class<T> modelClass) {
         Provider<? extends ViewModel> creator = creators.get(modelClass);
+
         if (creator == null) {
-            for (Map.Entry<Class<? extends ViewModel>, Provider<ViewModel>> entry : creators.entrySet()) {
+            for (Map.Entry<Class<? extends ViewModel>,
+                    Provider<ViewModel>> entry : creators.entrySet()) {
                 if (modelClass.isAssignableFrom(entry.getKey())) {
                     creator = entry.getValue();
                     break;
                 }
             }
         }
+
         if (creator == null) {
             throw new IllegalArgumentException("unknown model class " + modelClass);
         }
+
         try {
             return (T) creator.get();
         } catch (Exception e) {
